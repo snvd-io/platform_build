614a813fa73ce267a759e13c63610c2d08cceb36
Daniel Micay <daniel.micay@grapheneos.org>
2024-07-28 15:07:55 -0400

    simplify build date/number output

--- envsetup.sh
+++ envsetup.sh
@@ -2132,11 +2132,11 @@ addcompletions
 export LANG=C.UTF-8
 export _JAVA_OPTIONS=-XX:-UsePerfData
 export BUILD_DATETIME=$(cat ${OUT_DIR:-out}/build_date.txt 2>/dev/null || date -u +%s)
-echo "BUILD_DATETIME=$BUILD_DATETIME"
 export BUILD_NUMBER=$(cat ${OUT_DIR:-out}/soong/build_number.txt 2>/dev/null || date -u -d @$BUILD_DATETIME +%Y%m%d00)
-echo "BUILD_NUMBER=$BUILD_NUMBER"
 export BUILD_USERNAME=grapheneos
 export BUILD_HOSTNAME=grapheneos
+
+echo "export BUILD_DATETIME=$BUILD_DATETIME BUILD_NUMBER=$BUILD_NUMBER"
 
 alias adevtool='vendor/adevtool/bin/run'
 alias adto='vendor/adevtool/bin/run'
