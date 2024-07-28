035a20b72de53fab3b93dfc0ef71e16733c23329
Daniel Micay <daniel.micay@grapheneos.org>
2024-07-28 15:57:16 -0400

    drop _JAVA_OPTIONS
    This gets ignored by soong and would need to be set there if we still
    want to disable perfdata. The overhead is very low and it's likely not
    worth implementing this again.

--- envsetup.sh
+++ envsetup.sh
@@ -2130,7 +2130,6 @@ source_vendorsetup
 addcompletions
 
 export LANG=C.UTF-8
-export _JAVA_OPTIONS=-XX:-UsePerfData
 export BUILD_DATETIME=$(cat ${OUT_DIR:-out}/build_date.txt 2>/dev/null || date -u +%s)
 export BUILD_NUMBER=$(cat ${OUT_DIR:-out}/soong/build_number.txt 2>/dev/null || date -u -d @$BUILD_DATETIME +%Y%m%d00)
 export BUILD_USERNAME=grapheneos
